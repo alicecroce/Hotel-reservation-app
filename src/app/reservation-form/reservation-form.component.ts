21ea4426f388f49bc2a274062d5b9da9d5ce1b43
alicecroce <alicecroce2@gmail.com>
2024-08-21 17:44:58 +0200

    toggle disabled

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css'
 })
-export class ReservationFormComponent {
-reservationForm:FormGroup=new FormGroup({});
+export class ReservationFormComponent implements OnInit  {
 
-onSubmit(){
-  
-}
+  reservationForm:FormGroup=new FormGroup({});
+
+  constructor(private FormBuilder:FormBuilder){
+
+  }
+
+    ngOnInit(): void {
+      this.reservationForm=this.FormBuilder.group({
+        checkInDate:['',Validators.required],
+        checkOutDate:['',Validators.required],
+        guestName:['',Validators.required],
+        guestEmail:['',[Validators.required, Validators.email]],
+        roomNumber:['',Validators.required],
+      })
+    }
+
+    onSubmit(){
+      if(this.reservationForm.valid){
+        console.log("valid");
+      }
+    }
 }
