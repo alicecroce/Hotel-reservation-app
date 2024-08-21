92aabb651578fca8443521824b5c13db4e0b098a
alicecroce <alicecroce2@gmail.com>
2024-08-21 18:14:19 +0200

    CRUD

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservations:Reservation[]= [];
+
+  //CRUD
+
+  //restituisce tutte le prenotazioni: READ
+  getReservations():Reservation[]{
+    return this.reservations;
+  }
+
+  //cerchiamo una prenotazione specifica per id con il metodo find: READ
+  getReservation(id:string):Reservation| undefined{
+    return this.reservations.find(res=>res.id===id);
+  }
+
+  //pushiamo la prenotazione all'interno del sistema: CREATE
+  addReservation(reservation:Reservation):void{
+    this.reservations.push(reservation);
+  }
+
+  //cancelliamo la prenotazione: DELETE
+  deleteReservation(id:string):void{
+    let index=this.reservations.findIndex(res=>res.id===id);
+    this.reservations.splice(index,1);
+  }
+
+  //carichiamo la prenotazione a sistema: UPDATE
+  updateReservation(updateReservation:Reservation):void{
+    let index=this.reservations.findIndex(res=>res.id===updateReservation.id);
+    this.reservations[index]=updateReservation;
+  }
+
+
+
 }
