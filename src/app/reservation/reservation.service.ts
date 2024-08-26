a6c4f0ec8a1a303c085bdf6b09a6384330d66ed1
alicecroce <alicecroce2@gmail.com>
2024-08-26 15:43:00 +0200

    I eliminate local storage in favor of the use of a mockAPI

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,10 +8,7 @@ export class ReservationService {
 
   private reservations:Reservation[]= [];
 
-  constructor(){
-    let savedReservations=localStorage.getItem("reservations");
-    this.reservations=savedReservations?JSON.parse(savedReservations):[];
-  }
+
 
   //CRUD
 
@@ -29,21 +26,21 @@ export class ReservationService {
   addReservation(reservation:Reservation):void{
     reservation.id=Date.now().toString();//rendo l'ID univoco
     this.reservations.push(reservation);
-    localStorage.setItem("reservations",JSON.stringify(this.reservations)); 
+
   }
 
   //cancelliamo la prenotazione: DELETE
   deleteReservation(id:string):void{
     let index=this.reservations.findIndex(res=>res.id===id);
     this.reservations.splice(index,1);
-    localStorage.setItem("reservations",JSON.stringify(this.reservations)); 
+
   }
 
   //carichiamo la prenotazione a sistema: UPDATE
   updateReservation(id:string, updateReservation:Reservation):void{
     let index=this.reservations.findIndex(res=>res.id===updateReservation.id);
     this.reservations[index]=updateReservation;
-    localStorage.setItem("reservations",JSON.stringify(this.reservations)); 
+
   }
 
 
